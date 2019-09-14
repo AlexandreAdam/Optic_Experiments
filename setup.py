2c8f937f7a3d7847c28e519c6d785c0caadf502e
AdamAlexandre <adam.alexandre01123@gmail.com>
2019-09-14 13:22:25 -0400

    Created file structure, added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = "PHY3040",
+        version = "1.0",
+        packages = find_packages(exclude=['contrib', 'docs', 'tests']),
+        install_requires=[
+            "numpy==1.17.2"
+            ]
+        )
+        
